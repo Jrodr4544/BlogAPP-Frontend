393fd3061c6b32b01ac68c08bb009bf05bce4ea9
jrodr4544 <maskdbandit@gmail.com>
2020-01-29 15:08:33 -0500

    removed header link and added app name. Also included the navbar that's under construction and imported browserRouterand route from react router dom to begin the routing for app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
+
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h3
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            BLOGGIN
+          </h3>
+          
+        </header>
+        <Router>
+          {/* <NavBar /> */}
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
