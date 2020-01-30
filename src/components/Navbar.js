3ddc3b618be4c809fe7333fabaf31e6e19b0c698
jrodr4544 <maskdbandit@gmail.com>
2020-01-30 15:51:06 -0500

    fixed navbar links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,21 +4,29 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
     return (
-      <Nav justify variant="tabs" defaultActiveKey="/home">
-        <Nav.Item>
-          <Nav.Link href="/home">Home</Nav.Link>
-        </Nav.Item>
+      <Nav fill bg="dark" variant="tabs" >
         <Nav.Item>
           <Nav.Link eventKey="link-1">
-            <NavLink to="posts">Posts</NavLink>
+            <NavLink to="/">
+              Home  
+            </NavLink>
           </Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link eventKey="link-2">Link 2</Nav.Link>
+          <Nav.Link eventKey="link-2">
+            <NavLink to="/posts">
+              Posts
+            </NavLink>
+          </Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link eventKey="link-3">
-            <NavLink to="/about">About</NavLink>
+          <Nav.Link eventKey="link-3">Link</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-4"> 
+            <NavLink to="/about">
+              About
+            </NavLink>
           </Nav.Link>
         </Nav.Item>
       </Nav>
