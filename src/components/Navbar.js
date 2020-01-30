efb1e4090ce2f29cdef6a84cf89575a2c7148f8a
jrodr4544 <maskdbandit@gmail.com>
2020-01-29 19:31:17 -0500

    Modified Navbar to reflect bootstrap updates and added two links to two components.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,38 +4,24 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
     return (
-      <Navbar inverse collapseOnSelect>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <NavLink
-              to='/'
-            >
-              Mitre's Att&ck Patterns
-            </NavLink>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-          <Nav>
-              <Navbar.Brand>
-                <NavLink
-                  to="/attacks"
-                >
-                  Attacks 
-                </NavLink>
-              </Navbar.Brand>
-          </Nav>
-          <Nav pullRight>
-            <Navbar.Brand>
-                <NavLink
-                  to="/about"
-                > 
-                  About 
-                </NavLink>
-            </Navbar.Brand>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <Nav justify variant="tabs" defaultActiveKey="/home">
+        <Nav.Item>
+          <Nav.Link href="/home">Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1">
+            <NavLink to="posts">Posts</NavLink>
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-2">Link 2</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-3">
+            <NavLink to="/about">About</NavLink>
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
     )
 }
 
