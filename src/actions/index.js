4230e18802c6e7f5219d230ad8dab37cc11c555d
jrodr4544 <maskdbandit@gmail.com>
2020-02-04 22:34:09 -0500

    added actions and defined the first one for submitting a post

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,45 @@
 // define actions below
+// one for fetching posts
+// one for creating posts
+// one for submitting comments
+export const fetchPosts = () => {
+debugger
+  return async dispatch => {
+    function onSuccess(posts) {
+    //   let comments = {};
+
+    //   assign comments to post unless they are nested.
+    //   debugger
+      dispatch({ type: 'FETCH_POSTS', posts });
+    //   dispatch({ type: 'SET_COMMENTS', comments })
+    //    debugger 
+      return posts;
+    }
+
+    function onError(error) {
+      dispatch({ type: 'FETCH_ERROR_GENERATED', error });
+      return error;
+    }
+
+    try {
+      dispatch({ type: 'LOADING_POSTS' })
+        debugger
+    //   const request = await fetch('/api/posts');
+      const request = await fetch('http://localhost:3001/api/posts');
+      const posts = await request.json();
+
+      return onSuccess(posts);
+    } catch (error) {
+      return onError(error);
+    }
+  }
+
+}
+
+export const createPost = () => {
+
+}
+
+export const submitComment = () => {
+
+}
