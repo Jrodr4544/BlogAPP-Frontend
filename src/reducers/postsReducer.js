920166d2bdeae626f7872b3f20a9fe39777758c4
jrodr4544 <maskdbandit@gmail.com>
2020-02-04 22:37:16 -0500

    defined first few statements in posts reducer

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,4 +1,4 @@
-export default (state = [], action) => {
+export default (state = { loading: false, posts: [] }, action) => {
     let idx;
     switch (action.type) {
         case "SET_POSTS":
@@ -7,6 +7,25 @@ export default (state = [], action) => {
         console.log(action)
             return action
         
+        case "LOADING_POSTS":
+        debugger
+        console.log("LOADING POSTS")
+        console.log(action)
+            return { ...state, loading: true }
+
+        case "FETCH_POSTS":
+        // debugger
+        console.log('FETCHING_POSTS')
+        console.log(action)
+        console.log('POSTS FETCHED')
+            return { ...state, posts: action.posts }
+        
+        case "FETCH_ERROR_GENERATED":
+        debugger
+        console.log('FETCH_ERROR_GENERATED')
+        console.log(action)
+            return { ...state, loading: action } 
+
         default:
             return state
     }
