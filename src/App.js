af11b0037c00fbf29fa7787cd541eec8602aaab0
jrodr4544 <maskdbandit@gmail.com>
2020-02-17 18:13:45 -0500

    added route to new post which renders the NewPostContainer

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import './App.css';
 import NavBar from './components/Navbar';
 import About from './components/About';
 
+import NewPostContainer from './containers/newPostContainer'
 import PostsContainer from './containers/postsContainer'
 
 class App extends Component {
@@ -31,6 +32,7 @@ class App extends Component {
             <NavBar />
             <Route path='/posts' component={PostsContainer} />
             <Route path='/about' component={About} />
+            <Route path='/new_post' component={NewPostContainer} />
             <Route exact path="/" render={() => (
               <div className="App"><h1>Welcome</h1><img className="App-logo" src={logo} alt='171x180' /></div>
             )} />
