826a807764a1f8a7501fc14c693e85a9c5dffca9
jrodr4544 <maskdbandit@gmail.com>
2020-02-17 18:17:49 -0500

    added bootstrap card to display post details as well as a button that links to each individual post that renders

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import { Card, Button } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
 const Posts = ({ posts }) => {
     debugger
     const renderPosts = posts.map( post => 
-        <Card>
-        <Card.Header>{ post.title }</Card.Header>
-        <Card.Body>
-            <Card.Title>{ post.summary }</Card.Title>
-            <Card.Text>
-                { post.body }
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
+       <div>
+            <Card>
+                <Card.Header>{ post.title }</Card.Header>
+                <Card.Body>
+                <Card.Title>{ post.summary }</Card.Title>
+                <Card.Text>
+                    { post.body }
+                </Card.Text>
+                <Button variant="dark" >
+                    <NavLink to={`/posts/${post.id}`}>Go to Post</NavLink>
+                </Button>
+            </Card.Body>
+            </Card>
+            <br></br>
+        </div>
     );
+
     return (
         <div>POSTS COMPONENT
             {renderPosts}
