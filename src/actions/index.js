a23c13b29689a30159195440b15e63235e6cfee8
jrodr4544 <maskdbandit@gmail.com>
2020-02-21 16:43:29 -0500

    finalized deletePost and createPost functions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,4 @@
-// define actions below
-// one for fetching posts
-// one for creating posts
-// one for submitting comments
+
 export const fetchPosts = () => {
 debugger
   return async dispatch => {
@@ -12,7 +9,7 @@ debugger
     //   debugger
       dispatch({ type: 'FETCH_POSTS', posts });
     //   dispatch({ type: 'SET_COMMENTS', comments })
-    //    debugger 
+    //   debugger 
       return posts;
     }
 
@@ -23,8 +20,7 @@ debugger
 
     try {
       dispatch({ type: 'LOADING_POSTS' })
-        debugger
-    //   const request = await fetch('/api/posts');
+
       const request = await fetch('http://localhost:3001/api/posts');
       const posts = await request.json();
 
@@ -36,10 +32,77 @@ debugger
 
 }
 
-export const createPost = () => {
+export const createPost = (post) => {
+  return async dispatch => {
+    function onSuccess(post) {
+    
+      dispatch({ type: 'CREATE_POST', post });
 
+      return post;
+    }
+
+    function onError(error) {
+      dispatch({ type: 'POST_ERROR_GENERATED', error });
+      return error;
+    }
+
+    try {
+      dispatch({ type: 'LOADING_POSTS' })
+
+      const request = await fetch('http://localhost:3001/api/posts', {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+            body: JSON.stringify(post),
+      });
+      
+      const posts = await request.json();
+
+      return onSuccess(posts);
+    } catch (error) {
+      return onError(error);
+    }
+
+  }
 }
 
-export const submitComment = () => {
+export const submitComment = () => { 
+}
 
+export const deletePost = (postId) => {
+    return async dispatch => {
+      function onSuccess(updatedPosts) {
+      
+        dispatch({ type: 'DELETED_POST', updatedPosts });
+
+        return updatedPosts;
+      }
+  
+      function onError(error) {
+        dispatch({ type: 'POST_ERROR_GENERATED', error });
+        return error;
+      }
+  
+      try {
+        dispatch({ type: 'LOADING_POSTS' })
+
+        const request = await fetch('http://localhost:3001/api/posts/'+ postId, {
+          method: 'DELETE',
+          headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+          },
+              body: JSON.stringify({post_id: postId})
+        });
+        
+        const posts = await request.json();
+
+        return onSuccess(posts);
+      } catch (error) {
+        return onError(error);
+      }
+  
+    }
 }
